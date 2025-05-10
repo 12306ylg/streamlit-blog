758289761cc3dab2719a6090309668776a4a51c3
12306ylg <qwertyuiop9296@outlook.com>
2025-05-10 16:34:10 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,16 +1,24 @@
+import sys
 import threading
 import time
 import streamlit as st
 import os
 import config
 import admin
+import random
+
 def close():
     st.session_state["is_show"] = False
+
+def handle_close(content_f):
+    close()
+    content_f.close()
+
 def show_content(item: str):
-    st.session_state["content"]=item
+    st.session_state["content"] = item
     st.session_state["is_show"] = True
-    st.image(config.icon,width=50)
-    st.write(config.name,unsafe_allow_html=True)
+    st.image(config.icon, width=50)
+    st.write(config.name, unsafe_allow_html=True)
     st.markdown("---")
     content_f = open(f"./blog/{item}", "r", encoding="utf-8")
     content = content_f.read()
@@ -25,14 +33,21 @@ def show_content(item: str):
     elif content_f.name.endswith(".html"):
         st.write(content, unsafe_allow_html=True)
     elif content_f.name.endswith(".mp4"):
-        st.video(content_f.name) if st.checkbox(f"Play{content_f.name}") else None
+        if st.checkbox(f"Play {content_f.name}"):
+            st.video(content_f.name)
     elif content_f.name.endswith(".mp3"):
-        st.audio(content_f.name) if st.checkbox(f"Play{content_f.name}") else None
-    cls=lambda: close();content_f.close()
+        if st.checkbox(f"Play {content_f.name}"):
+            st.audio(content_f.name)
     
-    st.button("Close","cls_What_happend_of_the_button",on_click=cls)
-def setsssts(key,value):st.session_state[key]=value
+    st.button("Close", 
+              key=f"cls_{random.randint(1, sys.maxsize)}", 
+              on_click=lambda: handle_close(content_f))
+
+def setsssts(key,value):
+    st.session_state[key]=value
+
 class Comment:
+    @staticmethod
     def comment(content:str):
         def submit(name:str,email:str,comment:str):
             if st.session_state.get("commeted"):
@@ -49,6 +64,8 @@ class Comment:
         st.text_input("Name",key="name")
         st.text_input("Email",key="email")
         st.button("Submit",on_click=submit,args=(st.session_state["name"],st.session_state["email"],st.session_state["comment"]))
+
+    @staticmethod
     def show_comment(content:str):
         if os.path.isfile(f"./blog/{content}.comment"):
             with open(f"./blog/{content}.comment",encoding="utf-8") as f:
@@ -62,6 +79,7 @@ def head():
     st.header("HomePage!")
     st.info(time.ctime())
     st.info("page is in home.py")
+
 def body():
     directory = admin.PostIndex()#Get index of .index
     file: list[str] = []
@@ -71,23 +89,21 @@ def body():
             file.append(item)
         else:
             dirs.append(item)
+    
     def show(file: list[str]):
-     for item in file:
-        st.markdown("---")
-        st.markdown(f"## {item.split(".")[0]}")
-        #show cover image if it exists
-        image=item.split(".")[0]+".png" \
-        if os.path.isfile(f"./blog/{item.split('.')[0]}.png") else item.split(".")[0]+".jpg" \
-        if os.path.isfile(f"./blog/{item.split('.')[0]}.jpg") else item.split(".")[0]+".gif" \
-        if os.path.isfile(f"./blog/{item.split('.')[0]}.gif") else None
-        #============================================
-        if image:st.image(f"./blog/{item}",width=200)
-        st.markdown(f"**{open(f"./blog/{item}",encoding="utf-8",errors="ignore").read(100)}……**")
-        st.button("Read", on_click=show_content, args=(item,), key=item)
+        for item in file:
+            st.markdown("---")
+            st.markdown(f"## {item.split('.')[0]}")
+            #show cover image if it exists
+            image=item.split(".")[0]+".png" \
+            if os.path.isfile(f"./blog/{item.split('.')[0]}.png") else item.split(".")[0]+".jpg" \
+            if os.path.isfile(f"./blog/{item.split('.')[0]}.jpg") else item.split(".")[0]+".gif" \
+            if os.path.isfile(f"./blog/{item.split('.')[0]}.gif") else None
+            #============================================
+            if image:st.image(f"./blog/{image}",width=200)
+            st.markdown(f"**{open(f"./blog/{item}",encoding="utf-8",errors="ignore").read(100)}……**")
+            st.button("Read", on_click=show_content, args=(item,), key=item)
     show(file)
     for item in dirs:
         with st.expander(item):
             show([f"./{item}/"+i for i in os.listdir(f"./blog/{item}")])
-                    
-
-
